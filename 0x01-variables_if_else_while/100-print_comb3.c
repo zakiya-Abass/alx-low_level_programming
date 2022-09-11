35ea820908a9743cec2d6c6600d4e13459bec30f
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-11 14:43:56 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 1; digit1 < 10; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
