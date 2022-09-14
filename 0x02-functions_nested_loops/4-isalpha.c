a7e0fb2c7adb483c0183029468d99e6c61ca87a8
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-14 10:36:17 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _isalpha - checks wheatther or not a character is an alphabet
+ * _isalpha - checks whether or not a character is an alphabet
  * @c: ascii value
- * Retunrn: 1 if it is lowercase or 0 otherwise
- * */
+ * Return: 1 if it is lowercase or 0 otherwise
+ */
+
 
 int _isalpha(int c)
 {
 	if (( c >= 97 && <= 123) || (c >= 65 && c <= 91))
-{
-	return (1);
+	{
+		 return (1);
 }
 else
 {
