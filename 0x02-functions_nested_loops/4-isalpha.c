9755fa6004887cb4d047400cdaa3f0578d5e3d9d
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-14 11:07:09 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,14 +6,14 @@
  * Return: 1 if it is lowercase or 0 otherwise
  */
 
-int _isalpha(int c)
+int _isalphabet(int c)
 {
 	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 	{
-		 return (1);
+		return (1);
 	}
 	else
 	{
-		 return (0);
+		return (0);
 	}
 }
