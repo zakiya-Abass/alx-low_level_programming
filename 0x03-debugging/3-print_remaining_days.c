ca9a66d74c986768b5daae8736dba9e8ea15a1ce
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-15 14:35:24 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,38 +8,6 @@
 * @year: year
 * Return: void
 */
-Task 3.txt
-Who has access
-A
-System properties
-Type
-Text
-Size
-707 bytes
-Storage used
-707 bytes
-Location
-0x03. C - Debugging
-Owner
-Antony Bahati
-Modified
-Sep 14, 2022 by Antony Bahati
-Opened
-12:18 AM by me
-Created
-Sep 14, 2022
-No description
-Viewers can download
-#include "main.h"
-
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
 
 void print_remaining_days(int month, int day, int year)
 {
