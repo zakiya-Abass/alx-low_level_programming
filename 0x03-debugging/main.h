8a59520c222e6c66c4cb052ec6f73f156918ff4c
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-15 08:56:39 -0700

    task2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
-
-#endif /* MAIN_H */
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
