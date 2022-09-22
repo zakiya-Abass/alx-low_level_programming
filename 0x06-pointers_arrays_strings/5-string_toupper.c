ff75c1d6bcfdf8c26c3d55c5e3a83bc6bf771a46
zakiya-Abass <zakiyaabass@icloud.com>
2022-09-22 03:42:49 -0700

    Task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - converting any string to uppercase
+ * @x: string parameter
+ * Return: string
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
